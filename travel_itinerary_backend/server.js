85d706fc55c359a090325299c69059084704bdbb
Siddharth Kumar Mishra <140817732+Siddharthcoding@users.noreply.github.com>
2025-05-04 19:07:43 +0530

    Update server.js

--- travel_itinerary_backend/server.js
+++ travel_itinerary_backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors(https://travel-journey-backend.onrender.com));
+app.use(cors(https://trip-glide.onrender.com));
 app.use(express.json());
 
 
